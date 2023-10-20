fb0540c40348d3a9b7b3fc3533fb435074ac06fb
omsutariya <96515823+omsutariya@users.noreply.github.com>
2023-10-20 08:01:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def main():
   if st.button('Predict'):
     result = model.predict(inputs)
     updated_res = result.flatten().astype(int)
-    if updated_res == 0:
+    if updated_res <= 30:
        st.write("Not very Proabable you will have a stoke soon but still take good care of yourself regardless")
     else:
        st.write("It is Probable you might have a stroke soon therfore you should take better care of yourself")
